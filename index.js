fc86a577bd46745f66c18e9e1ef8a8197797ce53
JoelItaliano <92814212+JoelItaliano@users.noreply.github.com>
2022-06-24 17:02:25 -0300

    barra de usqueda creada sin efecto de cambio

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+buscador.addEventListener('click', () => {
+   buscador.classList.add('search_hidden')
+   let = valid
+})
+
+document.addEventListener('click', () => {
+
+    buscador.classList.remove('search_hidden')
+ })
