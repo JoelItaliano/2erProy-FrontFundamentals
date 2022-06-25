3761e90a19c0caf6dd6bf79fb71b87bfbcf9cdf8
JoelItaliano <92814212+JoelItaliano@users.noreply.github.com>
2022-06-25 15:59:04 -0300

    arreglando la barra de busqueda

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-
-
-
-
-buscador.addEventListener('click', () => {
-   buscador.classList.add('search_hidden')
-   let = valid
-})
+const buscadorHidden = document.getElementById('search_hidden');
+const buscador = document.getElementById('search')
+const container = document.getElementById('container')
 
 document.addEventListener('click', () => {
+   if (document.activeElement.type === 'search'){
+   //buscadorHidden.style.display = 'flex'
+      container.classList.add('input_Hidden')
+      
+   }else{
+      container.classList.remove('input_Hidden')
+   }
+})
 
-    buscador.classList.remove('search_hidden')
- })
+
+function enFoco (){
+   console.log('tengoFoco')
+}
+
+function sinFoco(){
+   console.log('yano')
+}
