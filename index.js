1eb8fe2977b6b1967773a4a5cdb0507897669c7b
JoelItaliano <92814212+JoelItaliano@users.noreply.github.com>
2022-06-25 16:58:20 -0300

    finalizacion de la bara de navegacion con los acomentarios agregados

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const buscadorHidden = document.getElementById('search_hidden');
 const buscador = document.getElementById('search')
 const container = document.getElementById('container')
+const lupaOculta = document.getElementById('lupa_oculta')
+const contBarraBusqueda = document.getElementById('conteiner_barra_busqueda')
 
+
+/**
+ * Evento de escucha de click, cuando esta selecc la barra de busqueda 
+ * realiza acciones de cambio de clase y cambios de estilos
+ */
 document.addEventListener('click', () => {
    if (document.activeElement.type === 'search'){
    //buscadorHidden.style.display = 'flex'
       container.classList.add('input_Hidden')
-      
+      lupaOculta.style.display = 'flex'
+      buscador.style.backgroundColor = 'rgb(0,0,0)'
+      contBarraBusqueda.style.marginLeft ='0%'
+      contBarraBusqueda.style.width = '81%'  
    }else{
       container.classList.remove('input_Hidden')
+      lupaOculta.style.display = 'none'
+      buscador.style.backgroundColor = 'rgb(17,17,17)'
+      contBarraBusqueda.style.marginLeft ='6%'
+      contBarraBusqueda.style.width = '75%'  
    }
 })
-
-
-function enFoco (){
-   console.log('tengoFoco')
-}
-
-function sinFoco(){
-   console.log('yano')
-}
